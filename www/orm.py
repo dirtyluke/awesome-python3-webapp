f5580019cf7bb5b3ad4b37bbe68100501baa8799
dirtyluke <lielaxu@gmail.com>
2017-05-03 13:16:52 +0800

    modify orm.py

--- www/orm.py
+++ www/orm.py
@@ -32,3 +32,54 @@ def select(sql,args,size=None):
         yield from cur.close()
         logging.info('rows returned:%s' % len(rs))
         return rs
+
+@asyncio.coroutine
+def execute(sql, args):
+    logging.info(sql, args)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?','%s'),args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+from orm import Model, StringField, IntegerField
+
+class User(Model):
+    __table__ = 'user'
+
+    id = IntegetField(primary_key=True)
+    name = StringField
+
+user = User(id=123,name='paris_yy')
+user.insert()
+users = User.findAll()
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self,key,None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key,str(value)))
+        return value
